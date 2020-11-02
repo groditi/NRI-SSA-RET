13006eb6b96f2d6a1950d01c59ab91d664a70084
Guillermo Roditi Dominguez <groditi@gmail.com>
2020-11-02 00:19:24 -0800

    medium post code and other charts

--- retirement.benefit.trends.R
+++ retirement.benefit.trends.R
@@ -19,8 +19,9 @@ transitions <- total_benes %>% filter(date >= ymd('1974-12-01')) %>%
   mutate(
     exits.ret.men = (lag(ret.men.count) - ret.men.count) + new.ret.men.count,
     exits.ret.women = (lag(ret.women.count) - ret.women.count) + new.ret.women.count,
+    exits.ret.total = (lag(ret.total.count) - ret.total.count) + new.ret.total.count,
     exits.spouse.men = (lag(spouse.men.count) - spouse.men.count) + new.spouse.men.count,
-    exits.spouse.women = (lag(spouse.women.count) - spouse.women.count) + new.spouse.women.count
+    exits.spouse.women = (lag(spouse.women.count) - spouse.women.count) + new.spouse.women.count,
   )
 
 #there is a definite seasonlity pattern to new awards so correct for that
@@ -84,3 +85,78 @@ zombie_plot <- transitions %>%
     theme( legend.position="bottom" )
 
 ggsave(file.path("etc", "ret.resusitations.png"), zombie_plot)
+
+#############################################################
+#the charts from the medium post
+transitions %>% 
+  filter(date >= ymd('1980-01-01') & date < ymd('2000-01-01')) %>% 
+  select(exits.ret.men, exits.ret.women, exits.spouse.men, exits.spouse.women) %>%
+  ts(frequency = 12, start = c(1980,1)) %>% autoplot + xlab("") + 
+  ylab("Extinguished SSA Retirement Beneficiaries") +
+  scale_y_continuous(labels = scales::comma) +
+  theme( legend.position="bottom" )
+
+transitions %>% 
+  filter(date >= ymd('1980-01-01') & date < ymd('2000-01-01')) %>% 
+  select(exits.ret.total) %>%
+  ts(frequency = 12, start = c(1980,1)) %>%
+  seas() %>% autoplot + xlab("") + 
+  ylab("Extinguished SSA Retirement Beneficiaries") +
+  scale_y_continuous(labels = scales::comma) 
+
+transitions %>% 
+  filter(date >= ymd('2001-01-01') & date < ymd('2020-01-01')) %>% 
+  select(exits.ret.total) %>%
+  ts(frequency = 12, start = c(2001,1)) %>%
+  seas() %>% autoplot + xlab("") + 
+  ylab("Extinguished SSA Retirement Beneficiaries") +
+  scale_y_continuous(labels = scales::comma) 
+
+predictor_model <- transitions %>% 
+  filter(date >= ymd('2001-01-01') & date < ymd('2020-01-01')) %>% 
+  select(exits.ret.total) %>%
+  ts(frequency = 12, start = c(2001,1)) %>%
+  seas( forecast.save = "fct" ) 
+
+forecasts_ts <- predictor_model %>% series('fct')
+exits_forecasts <- tibble(
+  date = as.Date(as.yearmon(index(forecasts_ts))),
+  forecast = forecasts_ts[,1],
+  lower.ci = forecasts_ts[,2],
+  upper.ci = forecasts_ts[,3]
+) 
+
+thisyearsucks <- transitions %>% 
+  select(date, exits.ret.total) %>%
+  filter(date >= ymd('2020-01-01')) %>% 
+  inner_join(exits_forecasts, by = 'date') %>%
+  mutate(
+    forecast_error = exits.ret.total-forecast,
+    cumulative_error = cumsum(forecast_error)
+  )
+   
+ 
+ci_plot <- thisyearsucks %>% 
+  ggplot(aes(x = date)) +
+  geom_ribbon(
+    aes(ymin = lower.ci, ymax = upper.ci),
+     alpha = 0.15, fill = '#0072B2'
+  ) +
+  geom_line(aes(y = forecast), color = "#666666", alpha = 0.5) +
+  geom_line(aes(y = exits.ret.total), color ="#D55E00", alpha = 0.7) +
+  theme(axis.title.x = element_blank(), axis.text.x = element_blank())
+  
+err_plot <- thisyearsucks %>% 
+  ggplot(aes(x = date)) +
+  geom_line(aes(y = forecast_error), alpha = 0.5, color ="#D55E00") +
+  theme(axis.title.x = element_blank(), axis.text.x = element_blank())
+
+cum_err_plot <- thisyearsucks %>% 
+  ggplot(aes(x = date)) +
+  geom_line(aes(y = cumulative_error), color ="#D55E00") +
+  theme(axis.title.x = element_blank())
+
+library(grid)
+grid.newpage()
+grid.draw(rbind(ggplotGrob(ci_plot), ggplotGrob(err_plot), ggplotGrob(cum_err_plot), size = "last"))
+
